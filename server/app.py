26f87a0d393b575820210617c9ca4dee0edbd3fa
Saki <sbasken@gmail.com>
2023-04-13 09:45:34 -0700

    Complete GET /restaurants/:id

--- server/app.py
+++ server/app.py
@@ -26,8 +26,18 @@ class Restaurants(Resource):
     def get(self):
         restaurants_dict = [ r.to_dict() for r in Restaurant.query.all() ]
         return restaurants_dict
+    
+class Restaurant_by_id(Resource):
+
+    def get(self, id):
+        found_restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if not found_restaurant:
+            return make_response({'error': 'Restaurant not found'}, 404)
+        return found_restaurant.to_dict(rules=('pizzas',))
+
 
 api.add_resource(Restaurants, '/restaurants')
+api.add_resource(Restaurant_by_id, '/restaurants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
