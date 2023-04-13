4d84014d171c1c94dd4d17f79e0fe55e8107f252
Saki <sbasken@gmail.com>
2023-04-13 09:09:10 -0700

    Set up tables

--- server/models.py
+++ server/models.py
@@ -11,3 +11,47 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    restraunt_pizzas = db.relationship("RestaurantPizza", backref="pizza")
+
+    def __repr__(self):
+        return f"<Pizza Name: {self.name}, Ingredients: {self.ingredients}>"
+    
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
+    pizza = db.relationship("Pizza", backref="restraunt_pizzas")
+    restaurant = db.relationship("Restaurant", backref="restraunt_pizzas")
+
+    def __repr__(self):
+        return f"<RestaurantPizza Price: {self.price}, Pizza: {self.pizza_id}, Restaurant: {self.restaurant_id}>"
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restraunt_pizzas = db.relationship("RestaurantPizza", backref="restaurant")
+
+    def __repr__(self):
+        return f"<Restaurant Name: {self.name}, Address: {self.address}>"
+                         
